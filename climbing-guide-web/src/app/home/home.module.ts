b2b5d83af6d1a5d21475a86a15af0b59984740c1
lyuboslav <lyubo.asenov@gmail.com>
2017-11-17 22:16:34 +0200

    Open info window per marker.

--- climbing-guide-web/src/app/home/home.module.ts
+++ climbing-guide-web/src/app/home/home.module.ts
@@ -1,5 +1,6 @@
 import { GuideMapModule } from '../guide-map/guide-map.module';
-import { HomeComponent, homeRoutes } from './';
+import { HomeComponent } from './home.component';
+import { homeRoutes } from './home.routes';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
